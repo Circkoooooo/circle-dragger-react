992e7602f3e640367aaa5ef6d8ee17bb7fb840c1
2389451262@qq.com <2389451262@qq.com>
2022-10-14 20:25:39 +0800

    fix: set ruler offset

--- src/Container/ViewContainer/useRuler.ts
+++ src/Container/ViewContainer/useRuler.ts
@@ -19,38 +19,38 @@ export const renderMark = (
 	context.lineWidth = 2;
 
 	if (direction === 'row') {
-		for (let i = 0; i < viewWidth; i += 10) {
+		for (let i = -80; i < viewWidth; i += 10) {
 			context.beginPath();
 			context.moveTo(i, 0);
-			context.lineTo(i, i % 100 === 0 ? longMark : shortMark);
+			context.lineTo(i, (i - 80) % 100 === 0 ? longMark : shortMark);
 			context.stroke();
 			context.closePath();
 		}
-		for (let i = 0; i < viewWidth; i += 10) {
+		for (let i = -80; i < viewWidth; i += 10) {
 			context.fillStyle = 'white';
-			if (i % 100 === 0) {
+			if (i - 80 >= 0 && (i - 80) % 100 === 0) {
 				context.save();
 				context.translate(5, 0);
-				context.fillText(i.toString(), i, 18);
+				context.fillText((i - 80).toString(), i, 18);
 				context.stroke();
 				context.restore();
 			}
 		}
 	} else {
-		for (let i = 0; i < viewHeight; i += 10) {
+		for (let i = -80; i < viewHeight; i += 10) {
 			context.beginPath();
 			context.moveTo(0, i);
-			context.lineTo(i % 100 === 0 ? longMark : shortMark, i);
+			context.lineTo((i - 80) % 100 === 0 ? longMark : shortMark, i);
 			context.stroke();
 			context.closePath();
 		}
-		for (let i = 0; i < viewHeight; i += 10) {
+		for (let i = -80; i < viewHeight; i += 10) {
 			context.fillStyle = 'white';
-			if (i % 100 === 0) {
+			if (i - 80 >= 0 && (i - 80) % 100 === 0) {
 				context.save();
 				context.translate(10, 5);
 				context.rotate((90 * Math.PI) / 180);
-				context.fillText(i.toString(), i, 0);
+				context.fillText((i - 80).toString(), i, 0);
 				context.restore();
 			}
 		}
