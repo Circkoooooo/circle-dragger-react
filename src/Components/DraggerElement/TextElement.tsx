803d78dc9f299ef8238afa49708e7246c9416510
2389451262@qq.com <2389451262@qq.com>
2022-10-15 21:43:40 +0800

    style: element style

--- src/Components/DraggerElement/TextElement.tsx
+++ src/Components/DraggerElement/TextElement.tsx
@@ -1,8 +1,19 @@
+import React from 'react';
 import { ElementType } from '../../types/ElementType';
+
+const textElementStyle = (): { [type: string]: React.CSSProperties } => {
+	return {
+		root: {
+			height: '20px',
+			width: '200px',
+			backgroundColor: '#fff',
+		},
+	};
+};
 
 export const TextElement = (): ElementType => {
 	return {
 		alias: '普通文本',
-		realComponent: <div>文本</div>,
+		realComponent: <input style={textElementStyle().root} />,
 	};
 };
