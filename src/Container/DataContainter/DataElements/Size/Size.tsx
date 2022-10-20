3c59a6216d2b3f8513138b37783517da7ec4227f
2389451262@qq.com <2389451262@qq.com>
2022-10-20 22:11:22 +0800

    fix: update Size component value name

--- src/Container/DataContainter/DataElements/Size/Size.tsx
+++ src/Container/DataContainter/DataElements/Size/Size.tsx
@@ -6,12 +6,12 @@ export const Size = (props: DataMap.Size) => {
 	return (
 		<div className='dataelement dataelement_size'>
 			<div className='element_item'>
-				<div className='item_name'>长度</div>
+				<div className='item_name'>宽度</div>
 				<div className='item_value'>{width ? width : '自动'}</div>
 			</div>
 
 			<div className='element_item'>
-				<div className='item_name'>宽度</div>
+				<div className='item_name'>高度</div>
 				<div className='item_value'>{height ? height : '自动'}</div>
 			</div>
 		</div>
